ea10b875574c580ddadef6aea36b17e0b15ebf5f
Emmanuelle Kamwa <emmanuellekamwa@gmail.com>
2024-01-22 17:56:12 +0100

    Crafted new questions for quizz app

---
+++ script.js
@@ -0,0 +1,28 @@
+const quizzData = [
+    {
+        question: 'What is the capital of USA?',
+        options: ['Paris', 'Washington DC', 'Yaoundé', 'Lisbonne'],
+        answer: 'Washington DC',
+    },
+    {
+        question: 'Which country won the FIFA World Cup in 2014?',
+        options: ['Portugal', 'Sweden', 'Germany', 'Switzerland'],
+        answer: 'Germany',
+    },
+    {
+        question: 'How many planets does the solar system have?',
+        options: ['3', '9', '6', '8'],
+        answer: '9',
+    },
+    {
+        question: 'What is the tallest mountain in the world?',
+        options: ['Kilimanjaro', 'Everest', 'Fako', 'Machupichu'],
+        answer: 'Everest',
+    },
+    {
+        question: 'Who is the creator of Microsoft?',
+        options: ['Steve Jobs', 'Elon Musk', 'Jeff Bezos', 'Bill Gates'],
+        answer: 'Bill Gates',
+    },
+];
+
